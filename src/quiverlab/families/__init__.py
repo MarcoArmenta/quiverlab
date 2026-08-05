3473d107b9357a32f144ebc218a64d5fb4a46f00
Marco Armenta <drmarcoarmenta@gmail.com>
2026-08-05 17:29:23 -0400

    feat(families): Potential + JacobianAlgebra(Q, W) -- cyclic derivatives, dim-certified, NotFiniteDimensionalError on Jacobian-infinite

--- src/quiverlab/families/__init__.py
+++ src/quiverlab/families/__init__.py
@@ -2,6 +2,7 @@ from quiverlab.families.basic import linear_path_algebra, truncated_polynomial  
 from quiverlab.families.dynkin import dynkin_quiver  # noqa: F401
 from quiverlab.families.exterior import ExteriorAlgebra  # noqa: F401
 from quiverlab.families.incidence import IncidenceAlgebra  # noqa: F401
+from quiverlab.families.jacobian import JacobianAlgebra, Potential, cyclic_derivative  # noqa: F401
 from quiverlab.families.nakayama import NakayamaAlgebra  # noqa: F401
 from quiverlab.families.one_point import OnePointExtension  # noqa: F401
 from quiverlab.families.path_algebra import PathAlgebra  # noqa: F401
